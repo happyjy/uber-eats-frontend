08b415e9b203b7f4e3b8d78a29713d7397d02d9d
happyjy <okwoyjy@gmail.com>
2021-03-11 10:00:10 +0900

    15.11 UI Clonning part Two

---
+++ src/components/button.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+interface IButtonProps {
+  canClick: boolean;
+  loading: boolean;
+  actionText: string;
+}
+
+export const Button: React.FC<IButtonProps> = ({
+  canClick,
+  loading,
+  actionText,
+}) => (
+  <button
+    className={`py-4 text-lg font-medium text-white focus:outline-none transition-colors ${
+      canClick
+        ? 'bg-lime-600 hover:bg-lime-700'
+        : 'bg-gray-300 pointer-events-none'
+    }`}
+  >
+    {loading ? 'Loading...' : actionText}
+  </button>
+);
