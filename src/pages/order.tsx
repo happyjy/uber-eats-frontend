0e4c84dd13a30c074704b4cb6c090b0d55c85567
happyjy <okwoyjy@gmail.com>
2021-03-27 13:17:42 +0900

    23.2 subscribeToMore

--- src/pages/order.tsx
+++ src/pages/order.tsx
@@ -49,81 +49,83 @@ export const Order = () => {
       },
     },
   );
-  const { data: subscriptionsData } = useSubscription<
-    orderUpdates,
-    orderUpdatesVariables
-  >(ORDER_SUBSCRIPTION, {
-    variables: {
-      input: {
-        id: +params.id,
-      },
-    },
-  });
+  // const { data: subscriptionsData } = useSubscription<
+  //   orderUpdates,
+  //   orderUpdatesVariables
+  // >(ORDER_SUBSCRIPTION, {
+  //   variables: {
+  //     input: {
+  //       id: +params.id,
+  //     },
+  //   },
+  // });
+  // console.log(subscriptionsData);
 
-  // useEffect(() => {
-  //   if (data?.getOrder.ok) {
-  //     // # subscriveToMore: query 이후 subscription을 사용하는 패턴이 많다보니 apollo client에서 미리 만들어 놓음
-  //     subscribeToMore({
-  //       document: ORDER_SUBSCRIPTION,
-  //       variables: {
-  //         input: {
-  //           id: +params.id,
-  //         },
-  //       },
-  //       updateQuery: (
-  //         prev,
-  //         {
-  //           subscriptionData: { data },
-  //         }: { subscriptionData: { data: orderUpdates } },
-  //       ) => {
-  //         if (!data) return prev;
-  //         return {
-  //           getOrder: {
-  //             ...prev.getOrder,
-  //             order: {
-  //               ...data.orderUpdates,
-  //             },
-  //           },
-  //         };
-  //       },
-  //     });
-  //   }
-  // }, [data]);
+  useEffect(() => {
+    if (data?.getOrder.ok) {
+      // # subscriveToMore: query 이후 subscription을 사용하는 패턴이 많다보니 apollo client에서 미리 만들어 놓음
+      subscribeToMore({
+        document: ORDER_SUBSCRIPTION,
+        variables: {
+          input: {
+            id: +params.id,
+          },
+        },
+        updateQuery: (
+          prev,
+          {
+            subscriptionData: { data },
+          }: { subscriptionData: { data: orderUpdates } },
+        ) => {
+          console.log("# subscribeToMore > updateQuery: ");
+          console.log({ prev, data });
 
-  console.log(subscriptionsData);
+          if (!data) return prev;
+          return {
+            getOrder: {
+              ...prev.getOrder,
+              order: {
+                ...data.orderUpdates,
+              },
+            },
+          };
+        },
+      });
+    }
+  }, [data]);
 
   return (
-    <div className="mt-32 container flex justify-center">
+    <div className="container flex justify-center mt-32">
       <Helmet>
         <title>Order #{params.id} | Uber Eats</title>
       </Helmet>
-      <div className="border border-gray-800 w-full max-w-screen-sm flex flex-col justify-center">
+      <div className="flex flex-col justify-center w-full max-w-screen-sm border border-gray-800 ">
         <h4 className="bg-gray-800 w-full py-5 text-white text-center text-xl">
           Order #{params.id}
         </h4>
-        <h5 className="p-5 pt-10 text-3xl text-center ">
+        <h5 className="p-5 pt-10 text-3xl text-center">
           ${data?.getOrder.order?.total}
         </h5>
-        <div className="p-5 text-xl grid gap-6">
-          <div className="border-t pt-5 border-gray-700">
+        <div className="grid gap-6 p-5 text-xl">
+          <div className="pt-5 border-t border-gray-700">
             Prepared By:{" "}
             <span className="font-medium">
               {data?.getOrder.order?.restaurant?.name}
             </span>
           </div>
-          <div className="border-t pt-5 border-gray-700 ">
+          <div className="pt-5 border-t border-gray-700">
             Deliver To:{" "}
             <span className="font-medium">
               {data?.getOrder.order?.customer?.email}
             </span>
           </div>
-          <div className="border-t border-b py-5 border-gray-700">
+          <div className="pt-5 py-5 border-t border-b border-gray-700">
             Driver:{" "}
             <span className="font-medium">
               {data?.getOrder.order?.driver?.email || "Not yet."}
             </span>
           </div>
-          <span className=" text-center mt-5 mb-3  text-2xl text-lime-600">
+          <span className="mt-5 mb-3 text-center text-2xl text-lime-600">
             Status: {data?.getOrder.order?.status}
           </span>
         </div>
